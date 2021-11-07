e83cd15c51b2169a5d107a90a017b4e40451a55b
Kelvin Tyrka Lopes <kelvintyrka@gmail.com>
2021-11-07 18:39:26 -0300

    First request

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <>
+    <>
         <NavBar />
         <div className="container">
             <div className="jumbotron">
